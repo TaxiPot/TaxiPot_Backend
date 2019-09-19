e61ef708daf0a21fd61bdafc3df87a2447a637d9
nwar-Jeon <nwar0825@naver.com>
2019-09-19 23:42:20 +0900

    Update TaxipotBackendApplication.java

--- src/main/java/kr/hs/dsm/java/taxipot_backend/TaxipotBackendApplication.java
+++ src/main/java/kr/hs/dsm/java/taxipot_backend/TaxipotBackendApplication.java
@@ -3,12 +3,11 @@ package kr.hs.dsm.java.taxipot_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 public class TaxipotBackendApplication {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/")
+    @RequestMapping(path = "/")
     String home() {
         return "Welcome Taxipot!";
     }
